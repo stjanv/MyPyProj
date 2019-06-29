16c700734fd88103ae83e1dc783476b49387965a
stjanv <ruslanstojnov@gmail.com>
2019-06-29 13:20:10 +0300

    up

--- second.py
+++ second.py
@@ -14,40 +14,40 @@ class Proxy(object):
 
 
 class ProxyServer(object):
-    ok = []
-    banned = []
+    def __init__(self):
+        self.ok = []
+        self.banned = []
 
-    @staticmethod
-    def next_proxy():
-        if ProxyServer.ok:
-            return ProxyServer.ok.pop(0)
+    def open_ip_list(self,f):
+        with open(f, 'r', encoding='utf-8') as file:
+            ip_list = [line.strip() for line in file]
+            for i in range(len(ip_list)):
+                t = tuple(item for item in ip_list[i].split(':'))
+                proxy = Proxy(t[0], t[1])
+                self.ok.append(proxy)
+            file.close()
+
+
+    def next_proxy(self):
+        if self.ok:
+            return self.ok.pop(0)
         else:
-            if ProxyServer.banned:
-                return ProxyServer.banned.pop(0)
+            if self.banned:
+                return self.banned.pop(0)
             else:
                 return 'ProxyServer is empty'
 
-    @staticmethod
-    def back_proxy(proxy):
+
+    def back_proxy(self, proxy):
         proxy.used_count += 1
         proxy.used_time = datetime.now()
         if proxy.status == 'Ok':
-            ProxyServer.ok.append(proxy)
+            self.ok.append(proxy)
         elif proxy.status == 'baned':
-            ProxyServer.banned.append(proxy)
+            self.banned.append(proxy)
 
 
-with open('./ip_list.txt', 'r', encoding='utf-8') as file:
-    ip_list = [line.strip() for line in file]
-    for i in range(len(ip_list)):
-        t = tuple(item for item in ip_list[i].split(':'))
-        proxy = Proxy(t[0], t[1])
-        ProxyServer.ok.append(proxy)
-    file.close()
 
-proxy_to_use = ProxyServer.next_proxy()
-print(proxy_to_use)
-proxy = Proxy("192.168.0.1","3312")
-ProxyServer.back_proxy(proxy)
-print(ProxyServer.ok[len(ProxyServer.ok) - 1])
+
+
 
